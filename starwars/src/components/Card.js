3c05d0162d3ea1259ecac07a28b7bb42db0c56ab
Michael Perez <mperez6473@gmail.com>
2020-03-20 11:36:51 -0500

    Feel like progress was just made. Got the name component to have a name for each different character. Mind you its filler but I can now add the name! wooooo

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Name from "./Name";
 export default function Card() {
     // https://swapi.co/api/people/
+    const [person, setPerson] = useState([]);
+
     useEffect(() => {
         axios
             .get("https://swapi.co/api/people/")
             .then(res => {
                 const peopleList = res.data.results;
                 console.log(peopleList);
-                updatePersons(peopleList);
+                setPerson(peopleList);
+                peopleList.map(val => console.log(val));
             })
             .catch(error =>
                 console.log(
@@ -18,6 +21,12 @@ export default function Card() {
                 )
             );
     }, []);
-    const [person, updatePersons] = useState({});
-    return <section></section>;
+    return (
+        <section>
+            {person.map(p => {
+                return <Name name={p.name} />;
+            })}
+            <Name />
+        </section>
+    );
 }
---
+++ starwars/src/components/Name.js
@@ -0,0 +1,5 @@
+import React from "react";
+export default function Name(props) {
+    console.log(props);
+    return <h3>Name Here</h3>;
+}
